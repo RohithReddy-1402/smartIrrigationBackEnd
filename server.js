183f61365f2613c16dcf5ed59784994f35995299
Rohith Kumar Reddy <111877972+RohithReddy-1402@users.noreply.github.com>
2025-02-27 21:40:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,40 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const HOST = "0.0.0.0"; 
-const cors=require("cors")
+const cors = require("cors");
+
 app.use(cors({
     origin: "*", 
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization"
-  }));
+}));
 app.use(express.json());
-let arr=[];
-let arr_data=[];
+let arr=[[0,0],[1,0],[2,0],[3,0]]
+let motor_status=[false,false,false,false]
+
+
 app.post("/send-data", (req, res) => {
-    console.log("Received data:", req.body);
-    arr.push(req.body); 
+    let x=req.body;
+    
+    for(let i=0;i<4;i++){
+        arr[i]=x.sensors[i];
+    }
+    
     res.json({ message: "Data received successfully" });
 });
-app.post("/data-transfer",(req,res)=>{
-    console.log("done !",req.body)
-    arr_data.push(req.body);
-    res.json({message:"Data done !"})
-})
+
+
 app.get("/get-command", (req, res) => {
     res.json(arr);
 });
-app.get("/motor-data",(req,res)=>{
-    res.json(arr_data.length>0?arr_data[arr_data.length]:arr_data );
-})
 
-app.listen(PORT, HOST, () => console.log(`Server running on http://${HOST}:${PORT}`));
+app.post("/motor-post",(req,res)=>{
+    let y=req.body;
+    motor_status=y
+
+})
+app.get("/motor-get",(req,res)=>{
+    res.json(motor_status);
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
